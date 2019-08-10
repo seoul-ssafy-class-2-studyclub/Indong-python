910770c15120dc5d8e40aa49c1b2ec0412e8093e
IndongLee <asghost@naver.com>
2019-08-10 10:16:58 +0900

    최대 상금 주석

--- D3/5293_이진 문자열 복원.py
+++ D3/5293_이진 문자열 복원.py
@@ -1,29 +1,48 @@
 def dfs(node, adj, path, vis, cnt=1):
-
-    vis[node] -= 1
-
+    visit = vis[:]
+    visit[node] -= 1
     path[cnt] = path[cnt-1] + [node]
 
     for i in range(0, len(adj[node])):
-        if vis[adj[node][i]]:
-            dfs(adj[node][i], adj, path, vis, cnt + 1)
+        if visit[adj[node][i]]:
+            dfs(adj[node][i], adj, path, visit, cnt + 1)
+
 
 def findpath(adj, vis, n):
-    path = [[] for i in range(sum(vis) + 1)]
+    length = sum(vis)
+    path = [[] for i in range(length + 1)]
     is_possible = False
+    
 
     for i in range(1, n + 1):
         visit = vis[:]
         if visit[i]:
             dfs(i, adj, path, visit)
-        if len(path[-1]) == sum(vis):
+        if len(path[-1]) == length:
             is_possible = True
             break
-    print(is_possible)
-    return path
+    if is_possible:
+        return path[-1]
+    else:
+        return False
 
 
 adj = [[], [1, 2], [3, 4], [1, 2], [3, 4]]
-vis = [0, 1, 2, 3, 4]
 
-print(findpath(adj, vis, 4))
+for case in range(1, int(input()) + 1):
+    vis = [0] + list(map(int, input().split()))
+    result = ''
+    path_list = findpath(adj, vis, 4)
+    if path_list:
+        for i in range(len(path_list)):
+            if path_list[i] <= 2:
+                result += '0'
+            else:
+                result += '1'
+        if path_list[-1] % 2 == 0:
+            result += '1'
+        else:
+            result += '0'
+        print(f'#{case} {result}')
+    else:
+        print(f'#{case} impossible')
