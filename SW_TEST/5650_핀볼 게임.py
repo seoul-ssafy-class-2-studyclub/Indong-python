0ac9b335591c5b52fee6e9519ac7f862a6243eff
IndongLee <asghost@naver.com>
2019-09-04 23:49:03 +0900

    0904

--- SW_TEST/5650_핀볼 게임.py
+++ SW_TEST/5650_핀볼 게임.py
@@ -1,40 +1,63 @@
-from pprint import pprint
-from collections import deque, defaultdict
-
-adj = [[], [1, 3, 0, 2], [3, 0, 1, 2], [2, 0, 3, 1], [1, 2, 3, 0], 
-[], [] ,[] ,[] ,[]]
-delta = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-blackhole = tuple()
-N = int(input())
-board = [list(map(int, input().split())) for _ in range(N)]
-point = defaultdict(lambda: [])
-for j in range(N):
-    for i in range(N):
-        if board[j][i] > 5:
-            adj[board[j][i]].append((j, i))
-        elif board[j][i] == -1:
-            blackhole = (j, i)
-        elif board[j][i] == 0:
-            for k in range(4):
-                point[(j, i)].append(k)
-            
-pprint(board)
-print(adj)
-print(point)
-print(blackhole)
-
-for key, value in point.items():
-    for i in range(4):
-        di = value[i]
-        if di == -1:
-            continue
-        y, x = key
-        yi, xi = y + delta[di][0], x + delta[di][1]
+def move(y, x, di):
+    cnt = 0
+    yi, xi = y + delta[di][0], x + delta[di][1]
+    while True:
         while 0 <= yi < N and 0 <= xi < N and not board[yi][xi]:
-            print((y, x), (yi, xi), di)
-            point[(yi, xi)][di] = -1
             yi += delta[di][0]
             xi += delta[di][1]
+            if yi == y and xi == x:
+                return cnt
+        cnt += 1
+        if 0 > xi or 0 > yi or xi >= N or yi >= N:
+            di = adj[5][di]
+            yi += delta[di][0]
+            xi += delta[di][1]
+        else:
+            com = board[yi][xi]
+            if com == -1:
+                cnt -= 1
+                return cnt
+            if com > 5:
+                di = di
+                cnt -= 1
+                if adj[com][0] == (yi, xi):
+                    yi, xi = adj[com][1]
+                else:
+                    yi, xi = adj[com][0]
+                yi += delta[di][0]
+                xi += delta[di][1]
+            else:
+                di = adj[com][di]
+                yi += delta[di][0]
+                xi += delta[di][1]
+        if yi == y and xi == x:
+            return cnt
+        
 
-print(point)
+for case in range(1, int(input()) + 1):
+    adj = [[], [1, 3, 0, 2], [3, 0, 1, 2], [2, 0, 3, 1], [1, 2, 3, 0], [1, 0, 3, 2], [], [], [], [], []]
+    delta = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    blackhole = tuple()
+    N = int(input())
+    board = [list(map(int, input().split())) for _ in range(N)]
+    point = []
 
+    for j in range(N):
+        for i in range(N):
+            if board[j][i] > 5:
+                adj[board[j][i]].append((j, i))
+            elif board[j][i] == -1:
+                blackhole = (j, i)
+            elif board[j][i] == 0:
+                point.append((j, i))
+
+    max_score = 0
+    for idx in point:
+        for i in range(4):
+            di = i
+            y, x = idx
+            score = move(y, x, di)
+            if max_score < score:
+                max_score = score
+
+    print(f'#{case} {max_score}')
