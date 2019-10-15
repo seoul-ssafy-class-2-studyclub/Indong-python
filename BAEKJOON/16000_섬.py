27b29a2a088999ae1ef5cea3cec1c22b0f7565ef
IndongLee <asghost@naver.com>
2019-10-15 19:10:23 +0900

    1015

--- BAEKJOON/16000_섬.py
+++ BAEKJOON/16000_섬.py
@@ -10,9 +10,18 @@ def bfs(cur):
         for dy, dx in dxy:
             yi = y + dy
             xi = x + dx
-            if 0 <= yi < N and 0 <= xi < M and pacific[yi][xi] == cur and not vis[yi][xi]:
-                queue.append((yi, xi))
-                vis[yi][xi] = num
+            if 0 <= yi < N and 0 <= xi < M:
+                nxt = vis[yi][xi]
+                if pacific[yi][xi] == cur and not nxt:
+                    queue.append((yi, xi))
+                    vis[yi][xi] = num
+                elif pacific[yi][xi] != cur and nxt:
+                    if con[nxt] == num:
+                        continue
+                    adj[num].append(nxt)
+                    adj[nxt].append(num)
+                    con[num] = num
+                    con[nxt] = num
 
 
 input = sys.stdin.readline
@@ -20,12 +29,15 @@ dxy = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 N, M = map(int, input().split())
 pacific = [list(input().strip()) for _ in range(N)]
 vis = [[False] * M for _ in range(N)]
+adj = [[]]
+con = [0] * 4000001
 islands = []
 ocean = []
 num = 1
 for r in range(N):
     for c in range(M):
         if not vis[r][c]:
+            adj.append([])
             bfs(pacific[r][c])
             if pacific[r][c] == '.':
                 ocean.append(num)
@@ -37,22 +49,9 @@ is_island = [False] * num
 for i in islands:
     is_island[i] = True
 
-# print(ocean)
-# print(islands)
-adj = [set() for _ in range(num)]
-for r in range(N - 1):
-    for c in range(M - 1):
-        cur = vis[r][c]
-        for dr, dc in dxy[1:3]:
-            ri = r + dr
-            ci = c + dc
-            nxt = vis[ri][ci]
-            if cur != nxt:
-                adj[nxt].add(cur)
-                adj[cur].add(nxt)
-
 tree = [[] for _ in range(num)]
 discovered = [False] * num
+safe = [True] * num
 is_cut = [False] * num
 order = 1
 def dfs_tree(node=1):
@@ -60,26 +59,21 @@ def dfs_tree(node=1):
     discovered[node] = order
     order += 1
     ret = discovered[node]
-    child = 0
     for i in adj[node]:
         nxt = discovered[i]
         if nxt:
             ret = min(ret, nxt)
             continue
         tree[node].append(i)
-        child += 1
-        prev = dfs_tree(i)
-    
-        if prev >= discovered[node]:
+        low = dfs_tree(i)
+        if low >= discovered[node]:
             is_cut[node] = True
-
-        ret = min(ret, prev)
+        ret = min(ret, low)
     
     return ret
 
 
 dfs_tree()
-safe = [True] * num
 visited = [False] * num
 def chk_unsafe(start):
     stack = deque()
@@ -92,7 +86,7 @@ def chk_unsafe(start):
             stack.append(child)
 
 
-for i in range(num):
+for i in range(1, num):
     if visited[i]:
         continue
     visited[i] = True
@@ -108,13 +102,5 @@ for r in range(N):
             pacific[r][c] = 'O'
         else:
             pacific[r][c] = 'X'
-            
+ 
     print(''.join(pacific[r]))
-
-    
-# print(is_island)
-# print(is_cut)
-# print(discovered)
-# print(tree)
-# print(adj)
-# print(safe)
--- BAEKJOON/17143_낚시왕.py
+++ BAEKJOON/17143_낚시왕.py
@@ -1,6 +1,7 @@
 import sys
 from pprint import pprint
 
+# 움직임이 끝났을 때 위치랑 방향 구하는 함수
 def calc(idx_s, w, speed, di):
     while True:
         idx_s += speed
@@ -23,6 +24,7 @@ def calc(idx_s, w, speed, di):
         
 
 R, C, M = map(int, sys.stdin.readline().split())
+# sharks: 상어들의 정보, board: 현재 상어들의 위치, chk: 이동한 상어들은 여기로 들어감
 sharks = []
 board = [[0 for i in range(C)] for _ in range(R)]
 chk = [[0 for i in range(C)] for _ in range(R)]
@@ -30,23 +32,29 @@ for i in range(M):
     r, c, s, d, z = map(int, sys.stdin.readline().split())
     r, c = r - 1, c - 1
     sharks.append([r, c, s, d, z])
+	# 복사(sharks[-1][:])가 아니라 참조!
     board[r][c] = sharks[-1]
 
 fish = 0
 for i in range(C):
+	# 낚시꾼이 상어 잡고
     for j in range(R):
         if board[j][i]:
             fish += board[j][i][4]
+		    # 죽은 상어는 크기를 -1로 바꿔줌. 위에서 복사가 아니라 참조로 해줬기 때문에 
+		    # board를 바꾸면 sharks 안의 값까지 같이 바꿔줄 수가 있다.
             board[j][i][4] = -1
             board[j][i] = 0
             break
 
     for j in range(len(sharks)):
         r, c, s, d, z = sharks[j]
+		# 죽은 상어면 지나가고
         if z == -1:
             continue
         board[r][c] = 0
-
+        
+		# 방향에 따라서 이동하게 만든다.
         if d == 1:
             r, d = calc(r, R - 1, -s, d)
         elif d == 2:
@@ -58,16 +66,19 @@ for i in range(C):
         sharks[j][0] = r
         sharks[j][1] = c
         sharks[j][3] = d
-
+        
+		# 이동하려는 곳에 아직 아무 상어도 없으면 그냥 집어넣고(이때도 복사가 아니라 참조)
         if not chk[r][c]:
             chk[r][c] = sharks[j]
+		# 상어가 있다면 크기를 비교해서 작은 상어의 크기를 -1로 바꿔준다.
         else:
             if chk[r][c][4] < sharks[j][4]:
+		        # 이때 chk[r][c][4]가 -1로 바뀌면서 sharks에 있는 녀석도 같이 바뀐다.
                 chk[r][c][4] = -1
                 chk[r][c] = sharks[j]
             else:
                 sharks[j][4] = -1
-
+    # board가 텅 비었을 테니 둘을 바꿔준다.
     board, chk = chk, board
 
 print(fish)
