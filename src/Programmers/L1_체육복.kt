e8fbbcd7334c3dc752a5737e70f8dabd1e39834d
IndongLee <kundera@kakao.com>
2020-03-04 22:20:12 +0900

    Solve 체육복

---
+++ src/Programmers/L1_체육복.kt
@@ -0,0 +1,31 @@
+package Programmers
+
+fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
+    var answer = 0
+    var clothes = Array<Int>(n) { 1 }
+    for (l in lost) --clothes[l-1]
+    for (r in reserve) ++clothes[r-1]
+    for (i in 0 until n) {
+        if (clothes[i] >= 1) {
+            ++answer
+            continue
+        }
+        when {
+            i - 1 >= 0 && clothes[i-1] == 2 -> {
+                --clothes[i-1]
+                ++clothes[i]
+                ++answer
+            }
+            i + 1 < n && clothes[i+1] == 2 -> {
+                --clothes[i+1]
+                ++clothes[i]
+                ++answer
+            }
+        }
+    }
+    return answer
+}
+
+fun main() {
+    println(solution(7, intArrayOf(2, 4, 6), intArrayOf(3, 6)))
+}
